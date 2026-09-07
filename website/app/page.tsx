feeb8fa72ad3c45f9e2f63d347d423c7a224c4c4
gabi <johana.gaba@epitech.eu>
2026-09-07 21:31:19 +0200

    add: starter pack new portfolio (again), homepage page fullscreen flex box container

---
+++ website/app/page.tsx
@@ -0,0 +1,26 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+
+    // Combine CSS Module class with Tailwind utilities using template literals
+    // <main className={`${styles.container} bg-slate-950 text-white p-8`}>
+    //   <h1 className="text-3xl font-bold tracking-tight">
+    //     Next.js + CSS Modules + Tailwind
+    //   </h1>
+    //   <p className="text-slate-400 mt-2">
+    //     Layout handled by CSS Modules, utilities handled by Tailwind.
+    //   </p>
+    // </main>
+
+    <main className={`${styles.fullscreen_container}  p-8`}>
+      <h1 className="text-3xl font-bold tracking-tight">
+       Johana Gaba
+     </h1>
+     <p className="text-slate-400 mt-2">
+      blablablablabla - to complete,, flex bow btw, everything needs to be responsive
+     </p>
+   </main>
+
+  );
+}
